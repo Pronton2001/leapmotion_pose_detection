d3628f8e716f00cf24209527140bcaf780eaca63
Pronton2001 <trihuynhtk15@gmail.com>
2021-03-14 00:50:32 +0700

    Saturday Night

--- fisrt temp/Assets/Script/getInfo.cs
+++ fisrt temp/Assets/Script/getInfo.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Leap.Unity;
 
+
+
 public class getInfo : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform[] getChild;
     public RigidHand rg_left; 
     public RigidHand rg_right; 
+
+	private	float[] product = new float[5]; 
+	private	float[] angle_ = new float[5];
+	private bool[] extend = new bool[5];
 
 
     void Start()
@@ -22,6 +28,9 @@ public class getInfo : MonoBehaviour
         rg_right= this.getChild[4].GetComponent<RigidHand>();
         Debug.Log(rg_left != null ?"rg_left is activate": "rg_left is null");
         Debug.Log(rg_right!= null ?"rg_right is activate": "rg_right is null");
+	for (int j = 0; j< HandModel.NUM_FINGERS; j++){
+		extend[j] = false;
+	}
     }
 
     // There are some basic gesture for classifying. Forward, Stop, Left, Right
@@ -31,12 +40,33 @@ public class getInfo : MonoBehaviour
         Left,
         Right
     }
+    private float getAngle(Vector3 tar, Vector3 src){
+	    float cos_theta = Vector3.Dot(tar, src) / (Mathf.Sqrt(Vector3.Dot(tar,tar)) * Vector3.Dot(src,src));
+	    return Mathf.Acos(cos_theta) * 180 / Mathf.PI;
+    }
     public void ClassifyHeuristic(){
-        
+	int counter = 0;
+	for (int i = 0; i< HandModel.NUM_FINGERS; ++i){
+		angle_[i] = getAngle( rg_left.fingers[i].GetBoneDirection(1),
+					rg_left.fingers[i].GetBoneDirection(2));
+		extend[i] = (angle_[i]<10) ? true : false;
+	}
+	for (int i = 0; i < HandModel.NUM_FINGERS; ++i){
+		if(extend[i]) counter++;
+		Debug.Log(rg_left.fingers[i].fingerType + " : " + angle_[i]);
+	}
+	Debug.Log("Number:"+ counter);
+
+        //Debug.Log("get" + rg_left.GetPalmDirection()+
+	//	   "get" + rg_left.GetPalmNormal());
     }
     void Update()
     {
-        for (int i=0; i<HandModel.NUM_FINGERS; i++){
+	//FingerInfo();   
+	ClassifyHeuristic();
+    }
+    void FingerInfo(){
+	for (int i=0; i<HandModel.NUM_FINGERS; i++){
             Debug.Log("LEFT:" + rg_left.fingers[i].fingerType + ":"+
                 rg_left.fingers[i].GetBoneDirection(FingerModel.NUM_BONES- 1));
         }
@@ -46,23 +76,6 @@ public class getInfo : MonoBehaviour
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // GetFingerJointStretchMecanim(int joint_type) {
 //  public Quaternion GetBoneRotation(int bone_type) {
